9ba4c1b192b56c7a1542926513067ad100da7088
TheDarkFly <mathiasroland95@gmail.com>
2024-06-22 00:57:47 +0200

    deleted all ignored import

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from country_list import available_languages, countries_for_language
-import os
 import time
 
 pays_dict = {}
